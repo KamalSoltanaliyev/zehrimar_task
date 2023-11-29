da293eb420c100adc7d9c33ee4565a30595ebdbc
KamalSoltanaliyev <148267364+KamalSoltanaliyev@users.noreply.github.com>
2023-11-29 19:09:45 +0400

    Add files via upload

---
+++ ZehirmarTask/ZehirmarTask/Program.cs
@@ -0,0 +1,10 @@
+﻿using ZehirmarTask;
+
+User user = new User();
+user.Id  = 1;
+user.Fullname = "Kamal Soltanaliyev";
+user.Email = "kamalsoltanaliyev@gmail.com";
+user.Password = "password@A12";
+
+user.ShowInfo(user.Id, user.Fullname, user.Email);
+Console.WriteLine(user.Password);
---
+++ ZehirmarTask/ZehirmarTask/User.cs
@@ -0,0 +1,53 @@
+﻿namespace ZehirmarTask
+{
+    internal class User
+    {
+        private string password;
+
+        public int Id { get; set; }
+        public string  Fullname { get; set; }
+        public string Email { get; set; }
+        
+        public string Password { get=> password; set
+            {
+                if (PasswordChecker(value))
+                {
+                    password = value;
+                }
+                else
+                {
+                    Console.WriteLine("ay xanim yer get");
+                }
+
+            } 
+        }
+
+        public bool PasswordChecker(string password)
+        {
+            if (password.Length < 8)
+            {
+                return false;
+            }
+            if (!password.Any(char.IsUpper))
+            {
+                return false;
+            }
+            if (!password.Any(char.IsLower))
+            {
+                return false;
+            }
+            if (!password.Any(char.IsNumber))
+            {
+                return false;
+            }
+            return true;
+        }
+
+
+        public void ShowInfo(int Id, string Fullname, string Email)
+        {
+            Console.WriteLine($"{Id} {Fullname} {Email}" );
+        }
+
+    }
+}
